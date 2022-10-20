ab4c9b1907a27a7eec30e5d9dd8838692ca66f02
Michael Skyba <michaelskyba@net-c.com>
2022-10-20 08:14:56 -0400

    update HTML for results

--- 2022/rps/index.ts
+++ 2022/rps/index.ts
@@ -2,15 +2,15 @@ const randomButton = document.getElementById("random")
 const redditButton = document.getElementById("reddit")
 const psychologyButton = document.getElementById("psychology")
 
-let gameMode: string;
+let gameMode: string
 
 randomButton.onclick = () => setGameMode("random")
 redditButton.onclick = () => setGameMode("reddit")
 psychologyButton.onclick = () => setGameMode("psychology")
 
 function setGameMode(mode: string) {
-	gameMode = mode;
+	gameMode = mode
 
-	document.getElementById("title").style.display = "none";
-	document.getElementById("game").style.display = "block";
+	document.getElementById("title").style.display = "none"
+	document.getElementById("game").style.display = "block"
 }
