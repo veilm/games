133dcc1ee4c782e7aa85ad3351b4dcbf99b7a0b0
Michael Skyba <michaelskyba@net-c.com>
2022-10-20 08:38:55 -0400

    screen switching

--- 2022/rps/index.ts
+++ 2022/rps/index.ts
@@ -1,16 +1,29 @@
-const randomButton = document.getElementById("random")
-const redditButton = document.getElementById("reddit")
-const psychologyButton = document.getElementById("psychology")
-
 let gameMode: string
 
-randomButton.onclick = () => setGameMode("random")
-redditButton.onclick = () => setGameMode("reddit")
-psychologyButton.onclick = () => setGameMode("psychology")
+const play = document.getElementById("play")
+const results = document.getElementById("results")
+
+document.getElementById("random").onclick = () => setGameMode("random")
+document.getElementById("reddit").onclick = () => setGameMode("reddit")
+document.getElementById("psychology").onclick = () => setGameMode("psychology")
+
+document.getElementById("rock").onclick = () => playOption("r")
+document.getElementById("paper").onclick = () => playOption("p")
+document.getElementById("scissors").onclick = () => playOption("s")
+
+document.getElementById("next").onclick = () => {
+	results.style.display = "none"
+	play.style.display = "block"
+}
 
 function setGameMode(mode: string) {
 	gameMode = mode
 
 	document.getElementById("title").style.display = "none"
 	document.getElementById("game").style.display = "block"
+}
+
+function playOption(option: string) {
+	play.style.display = "none"
+	results.style.display = "block"
 }
