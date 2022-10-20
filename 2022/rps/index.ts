1aef235ddc041a54557720d89f17641a0cafa267
Michael Skyba <michaelskyba@net-c.com>
2022-10-19 20:00:18 -0400

    Display options on mode choice

---
+++ 2022/rps/index.ts
@@ -0,0 +1,16 @@
+const randomButton = document.getElementById("random")
+const redditButton = document.getElementById("reddit")
+const psychologyButton = document.getElementById("psychology")
+
+let gameMode: string;
+
+randomButton.onclick = () => setGameMode("random")
+redditButton.onclick = () => setGameMode("reddit")
+psychologyButton.onclick = () => setGameMode("psychology")
+
+function setGameMode(mode: string) {
+	gameMode = mode;
+
+	document.getElementById("title").style.display = "none";
+	document.getElementById("game").style.display = "block";
+}
