c9740a14944d4aad6fd2463ac9e80bf44725e7ec
Michael Skyba <michaelskyba@net-c.com>
2022-11-05 16:23:51 -0400

    display and interaction POC

--- 2022/storytelling/main.ts
+++ 2022/storytelling/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!")
+function getId(id: string) {
+	return document.getElementById(id)
+}
+
+const b1 = getId("b1") as HTMLInputElement
+b1.onclick = () => {
+	getId("s1").style.display = "none"
+	getId("s2").style.display = "block"
+}
