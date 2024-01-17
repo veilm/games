f4c5b4faefad0620e418b263ac0f081ed0b85b3d
Michael Skyba <michaelskyba@net-c.com>
2024-01-17 11:28:27 -0500

    Basic canvas background

--- tools/simulation/main.ts
+++ tools/simulation/main.ts
@@ -1,1 +1,18 @@
-console.log("Hello, world!")
+const canvas = document.getElementById("canvas")! as HTMLCanvasElement
+const c = canvas.getContext("2d") as CanvasRenderingContext2D
+
+const frect = () => {
+	c.fillStyle = "maroon"
+	c.fillRect(0, 0, canvas.width, canvas.height)
+
+	console.log("Done")
+}
+
+const init = () => {
+	canvas.width = 1000
+	canvas.height = 500
+
+	frect()
+}
+
+init()
