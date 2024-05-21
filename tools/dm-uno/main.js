bff3a113986db0ad5e577231f9c4b59b442943fd
Michael Skyba <michaelskyba@net-c.com>
2024-05-21 13:45:23 -0400

    Implement spinner click

--- tools/dm-uno/main.js
+++ tools/dm-uno/main.js
@@ -8,7 +8,64 @@ const highlightSpinner = i => {
 	}
 }
 
-setInterval(() => {
+const makeSpin = () => {
 	selectedIdx = (selectedIdx + 1) % numColours;
 	highlightSpinner(selectedIdx);
-}, 150);
+}
+
+let spinInterval = setInterval(makeSpin, 150);
+
+const RNG = (min, max) =>
+	Math.round(Math.random() * (max - min)) + min;
+
+let target;
+let totalCycles;
+let cycles;
+
+let lastSpin = new Date().getTime();
+let spinThreshold = 10;
+
+// I just tweaked random values in desmos until it looked about right
+const getSpinTimeout = (cycles, totalCycles) => {
+	const x = (totalCycles - cycles)/totalCycles;
+
+	if (x < 0.2)
+		return 5;
+
+	return 1000 * Math.pow(x, 20 * x) + 20 * x;
+}
+
+const handleTime = () => {
+	const now = new Date().getTime();
+
+	const passed = now - lastSpin;
+	if (passed < getSpinTimeout(cycles, totalCycles))
+		return;
+
+	spinThreshold++;
+	makeSpin();
+	lastSpin = now;
+	cycles--;
+
+	if (cycles == 0)
+		clearInterval(spinInterval);
+}
+
+document.getElementById("spin").onclick = () => {
+	clearInterval(spinInterval);
+
+	totalCycles = 0;
+
+	// To get to 0
+	highlightSpinner(selectedIdx);
+	totalCycles = numColours - selectedIdx;
+
+	// Add some extra spins for visual effect
+	totalCycles += numColours * 20;
+
+	// We are at 0 still, so now we go to our target
+	totalCycles += RNG(0, numColours - 1);
+
+	cycles = totalCycles;
+	spinInterval = setInterval(handleTime, 10);
+}
